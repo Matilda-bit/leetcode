98e1c61bd5fa659b2546e93f890ba7302b7359be
Matilda-bit <polinaovras@gmail.com>
2024-01-23 15:10:04 +0200

    test commit

---
+++ leetcode/150_q/array_string/RemoveElement.php
@@ -0,0 +1,23 @@
+<?php
+
+class Solution {
+    function removeElement(&$nums, $val): int {
+        $k = 0;
+        foreach($nums as $num){
+            if($nums !== $val) {
+                $nums[$k] = $num;
+                $k++;
+            }
+        }
+        return $k;
+    }
+}
+
+//examples to run:
+$nums = [3,2,2,3];
+$val = 3;
+//or
+$nums = [0,1,2,2,3,0,4,2];
+$val = 2;
+
+?>
